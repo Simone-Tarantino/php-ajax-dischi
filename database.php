616920372662a9f45ecdd707adce843998b8c8d9
Simone Tarantino <questamailnonmiserveaniente@gmail.com>
2020-02-25 15:21:32 +0100

    WIP parte grafica

---
+++ database.php
@@ -0,0 +1,63 @@
+<?php
+  $database = [
+      [
+        'title' => 'New Jersey',
+        'author' => 'Bon Jovi',
+        'year' => 1988,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+      ],
+      [
+        'title' => 'Sempiternal',
+        'author' => 'Bring Me The Horizon',
+        'year' => 2013,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/91Z82BKqrSL._AC_SL1500_.jpg'
+      ],
+      [
+        'title' => 'Toxicity',
+        'author' => 'System Of A Down',
+        'year' => 2001,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81O5BH0QI4L._AC_SL1500_.jpg'
+      ],
+      [
+        'title' => 'Wolf',
+        'author' => 'Tyler, The Creator',
+        'year' => 2013,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81xs2JI9grL._AC_SL1500_.jpg'
+      ],
+      [
+        'title' => 'Blond',
+        'author' => 'Frank Ocean',
+        'year' => 2016,
+        'poster' => 'https://m.media-amazon.com/images/I/71l4jnxG29L._SS500_.jpg'
+      ],
+      [
+        'title' => 'Pure Heroine',
+        'author' => 'Lorde',
+        'year' => 2013,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/815U3MK2jrL._AC_SL1500_.jpg'
+      ],
+      [
+        'title' => 'Good Kid M.A.A.D. City',
+        'author' => 'Kendrick Lamar',
+        'year' => 2012,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/61cVkO%2BJveL._AC_.jpg'
+      ],
+      [
+        'title' => 'Torches',
+        'author' => 'Foster The People',
+        'year' => 2011,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81L7V3Q3rBL._AC_SL1500_.jpg'
+      ],
+      [
+        'title' => 'Cuore Amore Errore Disintegrazione',
+        'author' => 'Uochi Toki',
+        'year' => 2010,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/61p5YdymPVL._AC_.jpg'
+      ],
+      [
+        'title' => 'Viva La Vida',
+        'author' => 'Coldplay',
+        'year' => 2008,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/9145yafeO2L._AC_SL1500_.jpg'
+      ]
+  ];
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+  include __DIR__ . '/database.php'
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="dist/app.css">
+    <title>php-ajax-dischi</title>
+  </head>
+  <body>
+    <!-- header -->
+    <header>
+      <div class="header-container">
+        <img src="img/spotify-sketch.svg" alt="logo spotify">
+      </div>
+    </header>
+    <!-- /header -->
+    <!-- main -->
+    <main>
+
+    </main>
+    <!-- /main -->
+    <!-- footer -->
+    <footer>
+
+    </footer>
+    <!-- /footer -->
+  </body>
+</html>
